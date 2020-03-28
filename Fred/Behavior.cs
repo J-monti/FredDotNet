bfab95188f31e34502dcf7b1c2659ae940d01d4a
JMonti <jmontiverdi@gmail.com>
2020-03-28 10:03:14 -0500

    Updated solution

--- Fred/Behavior.cs
+++ Fred/Behavior.cs
@@ -36,7 +36,7 @@ namespace Fred
       this.HealthDecisionMaker = person;
       if (person != null)
       {
-        this.Intentions.Clear()
+        this.Intentions.Clear();
       }
     }
 
@@ -174,7 +174,7 @@ namespace Fred
 
     public bool adult_is_staying_home()
     {
-      int n = (int)BehaviorEnum.STAY_HOME_WHEN_SICK;
+      int n = (int)BehaviorEnum.StayHomeWhenSick;
       var my_intention = this.Intentions[n];
       if (my_intention == null)
       {
@@ -184,12 +184,12 @@ namespace Fred
       if (bParams.IsEnabled == false) {
         return false;
       }
-      return my_intention->agree();
+      return my_intention.agree();
     }
 
     public bool adult_is_taking_sick_leave()
     {
-      int n = (int)BehaviorEnum.TAKE_SICK_LEAVE;
+      int n = (int)BehaviorEnum.TakeSickLeave;
       var my_intention = this.Intentions[n];
       if (my_intention == null)
       {
@@ -199,112 +199,112 @@ namespace Fred
       if (bParams.IsEnabled == false) {
         return false;
       }
-      return my_intention->agree();
+      return my_intention.agree();
     }
 
     public bool child_is_staying_home()
     {
-      int n = (int)BehaviorEnum.KEEP_CHILD_HOME_WHEN_SICK;
+      int n = (int)BehaviorEnum.KeepChildHomeWhenSick;
       var bParams = BehaviorParams[n];
       if (bParams.IsEnabled == false) {
         return false;
       }
-      if (this->health_decision_maker != null)
+      if (this.health_decision_maker != null)
       {
-        return this->health_decision_maker->child_is_staying_home();
+        return this.health_decision_maker.child_is_staying_home();
       }
       // I am the health decision maker
       var my_intention = this.Intentions[n];
       assert(my_intention != null);
-      return my_intention->agree();
+      return my_intention.agree();
     }
 
     public bool acceptance_of_vaccine()
     {
-      int n = (int)BehaviorEnum.ACCEPT_VACCINE;
+      int n = (int)BehaviorEnum.AcceptVaccine;
       var bParams = BehaviorParams[n];
       if (bParams.IsEnabled == false) {
         return false;
       }
-      if (this->health_decision_maker != null)
+      if (this.health_decision_maker != null)
       {
-        // printf("Asking health_decision_maker %d \n", health_decision_maker->get_id());
-        return this->health_decision_maker->acceptance_of_vaccine();
+        // printf("Asking health_decision_maker %d \n", health_decision_maker.get_id());
+        return this.health_decision_maker.acceptance_of_vaccine();
       }
       // I am the health decision maker
       var my_intention = this.Intentions[n];
       assert(my_intention != null);
       // printf("My answer is:\n");
-      return my_intention->agree();
+      return my_intention.agree();
     }
 
     public bool acceptance_of_another_vaccine_dose()
     {
-      int n = (int)BehaviorEnum.ACCEPT_VACCINE_DOSE;
+      int n = (int)BehaviorEnum.AcceptVaccineDose;
       var bParams = BehaviorParams[n];
       if (bParams.IsEnabled == false) {
         return false;
       }
-      if (this->health_decision_maker != null)
+      if (this.health_decision_maker != null)
       {
-        return this->health_decision_maker->acceptance_of_another_vaccine_dose();
+        return this.health_decision_maker.acceptance_of_another_vaccine_dose();
       }
       // I am the health decision maker
       var my_intention = this.Intentions[n];
       assert(my_intention != null);
-      return my_intention->agree();
+      return my_intention.agree();
     }
 
     public bool child_acceptance_of_vaccine()
     {
-      int n = (int)BehaviorEnum.ACCEPT_VACCINE_FOR_CHILD;
+      int n = (int)BehaviorEnum.AcceptVaccineForChild;
       var bParams = BehaviorParams[n];
       if (bParams.IsEnabled == false) {
         return false;
       }
-      if (this->health_decision_maker != null)
+      if (this.health_decision_maker != null)
       {
-        printf("Asking health_decision_maker %d \n", this->health_decision_maker->get_id());
-        return this->health_decision_maker->child_acceptance_of_vaccine();
+        printf("Asking health_decision_maker %d \n", this.health_decision_maker.get_id());
+        return this.health_decision_maker.child_acceptance_of_vaccine();
       }
       // I am the health decision maker
       var my_intention = this.Intentions[n];
       assert(my_intention != null);
-      return my_intention->agree();
+      return my_intention.agree();
     }
 
     public bool child_acceptance_of_another_vaccine_dose()
     {
-      int n = (int)BehaviorEnum.ACCEPT_VACCINE_DOSE_FOR_CHILD;
+      int n = (int)BehaviorEnum.AcceptVaccineDoseForChild;
       var bParams = BehaviorParams[n];
       if (bParams.IsEnabled == false) {
         return false;
       }
-      if (this->health_decision_maker != null)
+      if (this.health_decision_maker != null)
       {
-        return this->health_decision_maker->child_acceptance_of_another_vaccine_dose();
+        return this.health_decision_maker.child_acceptance_of_another_vaccine_dose();
       }
       // I am the health decision maker
       var my_intention = this.Intentions[n];
       assert(my_intention != null);
-      return my_intention->agree();
+      return my_intention.agree();
     }
 
-    private Person select_adult(Household* h, int relationship, Person self)
+    private Person select_adult(Household h, int relationship, Person self)
     {
-      int N = h->get_size();
-      if (relationship == Global::GRANDCHILD)
+      int N = h.get_size();
+      if (relationship == Global.GRANDCHILD)
       {
 
         // select first adult natural child or spouse thereof of householder parent, if any
         for (int i = 0; i < N; ++i)
         {
-          Person person = h->get_enrollee(i);
-          if (person->is_adult() == false || person == self)
+          Person person = h.get_enrollee(i);
+          if (person.is_adult() == false || person == self)
           {
             continue;
           }
-          int r = person->get_relationship();
+          int r = person.get_relationship();
           if (r == Global::SPOUSE || r == Global::CHILD || r == Global::SIBLING || r == Global::IN_LAW)
           {
             return person;
@@ -314,12 +314,12 @@ namespace Fred
         // consider adult relative of householder, if any
         for (int i = 0; i < N; ++i)
         {
-          Person* person = h->get_enrollee(i);
-          if (person->is_adult() == false || person == self)
+          Person* person = h.get_enrollee(i);
+          if (person.is_adult() == false || person == self)
           {
             continue;
           }
-          int r = person->get_relationship();
+          int r = person.get_relationship();
           if (r == Global::PARENT || r == Global::OTHER_RELATIVE)
           {
             return person;
@@ -330,12 +330,12 @@ namespace Fred
       // select householder if an adult
       for (int i = 0; i < N; ++i)
       {
-        Person* person = h->get_enrollee(i);
-        if (person->is_adult() == false || person == self)
+        Person* person = h.get_enrollee(i);
+        if (person.is_adult() == false || person == self)
         {
           continue;
         }
-        if (person->get_relationship() == Global::HOUSEHOLDER)
+        if (person.get_relationship() == Global::HOUSEHOLDER)
         {
           return person;
         }
@@ -344,30 +344,30 @@ namespace Fred
       // select householder's spouse if an adult
       for (int i = 0; i < N; ++i)
       {
-        Person* person = h->get_enrollee(i);
-        if (person->is_adult() == false || person == self)
+        Person* person = h.get_enrollee(i);
+        if (person.is_adult() == false || person == self)
         {
           continue;
         }
-        if (person->get_relationship() == Global::SPOUSE)
+        if (person.get_relationship() == Global::SPOUSE)
         {
           return person;
         }
       }
 
       // select oldest available person
-      int max_age = self->get_age();
+      int max_age = self.get_age();
 
       Person* oldest = null;
       for (int i = 0; i < N; ++i)
       {
-        Person* person = h->get_enrollee(i);
-        if (person->get_age() <= max_age || person == self)
+        Person* person = h.get_enrollee(i);
+        if (person.get_age() <= max_age || person == self)
         {
           continue;
         }
         oldest = person;
-        max_age = oldest->get_age();
+        max_age = oldest.get_age();
       }
       return oldest;
     }
@@ -378,50 +378,50 @@ namespace Fred
       {
         return;
       }
-      if (this->health_decision_maker != null)
+      if (this.health_decision_maker != null)
       {
         return;
       }
       if (Global::Verbose > 1)
       {
-        printf("terminating behavior for agent %d age %d\n", self->get_id(), self->get_age());
+        printf("terminating behavior for agent %d age %d\n", self.get_id(), self.get_age());
       }
 
       // see if this person is the adult decision maker for any child in the household
-      Household* hh = static_cast<Household*>(self->get_household());
+      Household* hh = static_cast<Household*>(self.get_household());
       if (hh == null)
       {
-        if (Global::Enable_Hospitals && self->is_hospitalized() && self->get_permanent_household() != null)
+        if (Global::Enable_Hospitals && self.is_hospitalized() && self.get_permanent_household() != null)
         {
-          hh = static_cast<Household*>(self->get_permanent_household()); ;
+          hh = static_cast<Household*>(self.get_permanent_household()); ;
         }
       }
-      int size = hh->get_size();
+      int size = hh.get_size();
       for (int i = 0; i < size; ++i)
       {
-        Person* child = hh->get_enrollee(i);
-        if (child != self && child->get_health_decision_maker() == self)
+        Person* child = hh.get_enrollee(i);
+        if (child != self && child.get_health_decision_maker() == self)
         {
           if (Global::Verbose > 1)
           {
-            printf("need new decision maker for agent %d age %d\n", child->get_id(), child->get_age());
+            printf("need new decision maker for agent %d age %d\n", child.get_id(), child.get_age());
           }
-          child->setup_behavior();
+          child.setup_behavior();
           if (Global::Verbose > 1)
           {
-            printf("new decision maker is %d age %d\n", child->get_health_decision_maker()->get_id(),
-             child->get_health_decision_maker()->get_age());
+            printf("new decision maker is %d age %d\n", child.get_health_decision_maker().get_id(),
+             child.get_health_decision_maker().get_age());
             fflush(stdout);
           }
         }
       }
     }
 
-    void become_health_decision_maker(Person* self)
+    void become_health_decision_maker(Person self)
     {
-      if (this->health_decision_maker != null)
+      if (this.health_decision_maker != null)
       {
-        this->health_decision_maker = null;
+        this.health_decision_maker = null;
         delete_intentions();
         setup_intentions(self);
       }
